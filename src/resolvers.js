4348fa0c1ec881b776e1c00fc0aa6ed8c1f42ee5
John Claude <johnc@korbicom.com>
2022-08-14 17:36:18 +0200

    fixes

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,18 +1,22 @@
 const resolvers = {
   Query: {
-    // get all tracks, will be used to populate the homepage grid of our web client
+    // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
-    // get a specific track
+
+    // get a single track by ID, for the track page
     track: (_, { id }, { dataSources }) => {
       return dataSources.trackAPI.getTrack(id);
     },
+
+    // get a single module by ID, for the module detail page
     module: (_, { id }, { dataSources }) => {
       return dataSources.trackAPI.getModule(id);
     },
   },
   Mutation: {
+    // increments a track's numberOfViews property
     incrementTrackViews: async (_, { id }, { dataSources }) => {
       try {
         const track = await dataSources.trackAPI.incrementTrackViews(id);
@@ -36,9 +40,14 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
+
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    durationInSeconds: ({ length }) => length,
+  },
+  Module: {
+    durationInSeconds: ({ length }) => length,
   },
 };
 
--- src/schema.js
+++ src/schema.js
@@ -2,51 +2,75 @@ const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
-    "Get tracks array for homepage grid"
+    "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
-    track(id: ID!): Track
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track!
     "Fetch a specific module, provided a module's ID"
     module(id: ID!): Module!
   }
 
   type Mutation {
-    incrementTrackViews(id: ID!): IncrementTrackViewsResponse! 
+    "Increment the number of views of a given track, when the track card is clicked"
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
   }
 
   type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
     code: Int!
+    "Indicates whether the mutation was successful"
     success: Boolean!
+    "Human-readable message for the UI"
     message: String!
+    "Newly updated track after a successful mutation"
     track: Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
+    "The track's title"
     title: String!
+    "The track's main Author"
     author: Author!
-    "Thumbnail will contain a URL string"
+    "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    length: Int
+    "The track's approximate length to complete, in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
+    "The track's full duration (time it takes to complete), in seconds"
+    durationInSeconds: Int
+    "The number of modules this track contains"
     modulesCount: Int
-    numberOfViews: Int
+    "The track's complete description, can be in markdown format"
     description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
     modules: [Module!]!
   }
 
-  type Module {
-    id: ID!
-    title: String!
-    content: String
-    videoUrl: String
-    length: Int
-  }
-
-  "The author creates a learning track"
+  "Author of a complete Track or a Module"
   type Author {
     id: ID!
+    "Author's first and last name"
     name: String!
+    "Author's profile picture"
     photo: String
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The module's title"
+    title: String!
+    "The module's length in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
+    "The module's full video duration, in seconds"
+    durationInSeconds: Int
+    "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
+    content: String
+    "The module's video url, for video-based modules"
+    videoUrl: String
   }
 `;
 
